8b8c50912cb9e051bb2ae3818198f6851f804c3e
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2018-05-14 19:39:06 -0300

    now on git

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple discord bot as a tribute to my pretty friend John, also has a interrupt function that wil play a sound as someone selected starts to talk.
+
--- commands.js
+++ commands.js
@@ -1,163 +1,163 @@
-const bigTexts = require('./bigText.json');
+	const bigTexts = require('./bigText.json');
 
 
-var prettyJohn = function prettyJohn(message,args,options) {
+	var prettyJohn = function prettyJohn(message,args,options) {
 
-    if (options.prettyAllow) {
+	    if (options.prettyAllow) {
 
-        var text = message.content;
+	        var text = message.content;
 
-        if (text.toLowerCase().indexOf("joao") != -1 || text.toLowerCase().indexOf("joão") != -1  || text.toLowerCase().indexOf("lindo") != -1 ) {
-            message.channel.send("joao voce e lindoo");
-        } else if (text.toLowerCase().indexOf("john") != -1 || text.toLowerCase().indexOf("beautiful") != -1 ) {
-            message.channel.send("john u r beatiful");
-        }
+	        if (text.toLowerCase().indexOf("joao") != -1 || text.toLowerCase().indexOf("joão") != -1  || text.toLowerCase().indexOf("lindo") != -1 ) {
+	            message.channel.send("joao voce e lindoo");
+	        } else if (text.toLowerCase().indexOf("john") != -1 || text.toLowerCase().indexOf("beautiful") != -1 ) {
+	            message.channel.send("john u r beatiful");
+	        }
 
-    }
-};
+	    }
+	};
 
-var toggleAllow = function toggleAllow(message,args,options) {
+	var toggleAllow = function toggleAllow(message,args,options) {
 
-    options.prettyAllow = !options.prettyAllow;
+	    options.prettyAllow = !options.prettyAllow;
 
-    if (options.prettyAllow) {
-        message.channel.send(";)");
-    } else {
-         message.channel.send(":(");
-    }
-};
-
-
-
-var getRandText = function getRandText(message) {
-
-    var texts = Object.keys(bigTexts);
-    var key = texts[texts.length * Math.random() << 0];
-    var text = bigTexts[key];
-
-    message.channel.send( text );
-
-};
-
-
-var getTextKeys = function getTextKeys(message) {
-    var keys = Object.keys(bigTexts);
-    var msg = "Textoes : \n";
-    keys.forEach(key => {
-        msg += `    ${key}\n`
-    });
-    message.channel.send(msg);
-};
+	    if (options.prettyAllow) {
+	        message.channel.send(";)");
+	    } else {
+	         message.channel.send(":(");
+	    }
+	};
 
 
 
+	var getRandText = function getRandText(message) {
 
-var getText = function getText(message, args) {
-    if (bigTexts[args[0]]) {
-        message.channel.send(bigTexts[args[0]]); 
-    } else {
-        message.channel.send(":dizzy_face: Esse texto não foi cadastrado :dizzy_face:");
-    }
-};
+	    var texts = Object.keys(bigTexts);
+	    var key = texts[texts.length * Math.random() << 0];
+	    var text = bigTexts[key];
 
-var kick = function kick(message) {
+	    message.channel.send( text );
 
-    var member = message.mentions.members.first();
-
-    if (typeof member == "undefined")  {
-        message.channel.send(":scream: :scream: Este usuário não existe :scream: :scream: ")
-        return;
-    }
-
-    var nickname = Object.assign({}, member).user.username;
-    
-    member.kick().then(function() {
-        message.channel.send(`:hand_splayed: :hand_splayed:  xau xau ${nickname}`);
-    });
-
-};
+	};
 
 
-var interruptAllow = function interrupt(message,args,options) {
-
-
-	var member = message.mentions.members.first();
+	var getTextKeys = function getTextKeys(message) {
+	    var keys = Object.keys(bigTexts);
+	    var msg = "Textoes : \n";
+	    keys.forEach(key => {
+	        msg += `    ${key}\n`
+	    });
+	    message.channel.send(msg);
+	};
 
 
 
 
-	if (!member || !member.voiceChannel) return;
+	var getText = function getText(message, args) {
+	    if (bigTexts[args[0]]) {
+	        message.channel.send(bigTexts[args[0]]); 
+	    } else {
+	        message.channel.send(":dizzy_face: Esse texto não foi cadastrado :dizzy_face:");
+	    }
+	};
+
+	var kick = function kick(message) {
+
+	    var member = message.mentions.members.first();
+
+	    if (typeof member == "undefined")  {
+	        message.channel.send(":scream: :scream: Este usuário não existe :scream: :scream: ")
+	        return;
+	    }
+
+	    var nickname = Object.assign({}, member).user.username;
+	    
+	    member.kick().then(function() {
+	        message.channel.send(`:hand_splayed: :hand_splayed:  xau xau ${nickname}`);
+	    });
+
+	};
 
 
-	if (options.isInterrupting) {
-		if (options.interruptChannelID == member.voiceChannel.id) {
+	var interruptAllow = function interrupt(message,args,options) {
 
-			if (!options.interruptIDs.includes(member.id)) {
-				options.interruptIDs.push(member.id);
-				message.channel.send(`:smiling_imp:  <@${member.id}>`)
+
+		var member = message.mentions.members.first();
+
+
+
+
+		if (!member || !member.voiceChannel) return;
+
+
+		if (options.isInterrupting) {
+			if (options.interruptChannelID == member.voiceChannel.id) {
+
+				if (!options.interruptIDs.includes(member.id)) {
+					options.interruptIDs.push(member.id);
+					message.channel.send(`:smiling_imp:  <@${member.id}>`)
+
+				} else {
+					var index = options.interruptIDs.indexOf(member.id);
+					options.interruptIDs.splice(index,1);
+					message.channel.send(`:angel:   <@${member.id}>`);
+
+					if (options.interruptIDs.length == 0 ) {
+						options.isInterrupting = false;
+						member.voiceChannel.leave();
+					}
+				}
 
 			} else {
-				var index = options.interruptIDs.indexOf(member.id);
-				options.interruptIDs.splice(index,1);
-				message.channel.send(`:angel:   <@${member.id}>`);
 
-				if (options.interruptIDs.length == 0 ) {
-					options.isInterrupting = false;
-					member.voiceChannel.leave();
-				}
+				message.channel.send(`Já estou atrapalhando em outro canal`);
 			}
 
 		} else {
-
-			message.channel.send(`Já estou atrapalhando em outro canal`);
+			options.isInterrupting = true;
+			options.interruptChannelID = member.voiceChannel.id;
+			options.interruptIDs.push(member.id);
+			message.channel.send(`:smiling_imp:  <@${member.id}>`);
+			member.voiceChannel.join().then(connection => {
+				connection.on('speaking', user =>  {
+					if (options.interruptIDs.includes(user.id)) {
+						    const dispatcher = connection.playFile('./eaiMens.ogg');
+						    dispatcher.on('start',() => {
+						       connection.player.streamingData.pausedTime = 0;
+						    });
+					}
+				});
+			});
 		}
 
-	} else {
-		options.isInterrupting = true;
-		options.interruptChannelID = member.voiceChannel.id;
-		options.interruptIDs.push(member.id);
-		message.channel.send(`:smiling_imp:  <@${member.id}>`);
-		member.voiceChannel.join().then(connection => {
-			connection.on('speaking', user =>  {
-				if (options.interruptIDs.includes(user.id)) {
-					    const dispatcher = connection.playFile('./eaiMens.ogg');
-					    dispatcher.on('start',() => {
-					       connection.player.streamingData.pausedTime = 0;
-					    });
-				}
-			});
-		});
+	};
+
+
+	var play = function play (message,args,options) {
+		var broadcast = options.broadcast;
+		for (const connection of options.voiceConnections.values()) {
+			connection.playFile('./eaiMens.ogg');
+	  	}
 	}
 
-};
 
 
-var play = function play (message,args,options) {
-	var broadcast = options.broadcast;
-	for (const connection of options.voiceConnections.values()) {
-		connection.playFile('./eaiMens.ogg');
-  	}
-}
+	 
 
 
+	exports.any = {};
+	exports.adm = {};
+	//commands
+		//any
+	exports.any.textao = getText;
+	exports.any.randtext = getRandText;
+	exports.any.textkeys = getTextKeys;
+	exports.any.play = play;
+		//adm
+	exports.adm.allow = toggleAllow;
+	exports.adm.kick = kick;
+	exports.adm.interrupt = interruptAllow;
 
- 
-
-
-exports.any = {};
-exports.adm = {};
-//commands
-	//any
-exports.any.textao = getText;
-exports.any.randtext = getRandText;
-exports.any.textkeys = getTextKeys;
-exports.any.play = play;
-	//adm
-exports.adm.allow = toggleAllow;
-exports.adm.kick = kick;
-exports.adm.interrupt = interruptAllow;
-
-//others 
-exports.any.prettyJohn = prettyJohn;
+	//others 
+	exports.any.prettyJohn = prettyJohn;
 
 
